7cc9c51829a612f8309fbbec50f8df6b1ce3cdb2
zer0nim <marinernes@hotmail.fr>
2016-11-17 19:12:28 +0100

    corr

--- view/inscription.view.php
+++ view/inscription.view.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Le site de planification de mariage">
-    <link rel="stylesheet" href="../view/css/inscription.css" type="text/css" />
-
     <title> Connexion / Inscription</title>
+    <meta name="description" content="Le site de planification de mariage">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../view/css/inscription.css" type="text/css" />
 
   </head>
 
@@ -63,7 +64,5 @@
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   </body>
 </html>
