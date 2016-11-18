6b13e31b472fc7bd0a604698725c450bcf6268f2
Getkey <getkey@getkey.eu>
2016-11-18 14:59:07 +0100

    Met la page d'inscription en flexbox

--- view/inscription.view.php
+++ view/inscription.view.php
@@ -1,65 +1,63 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>MyWedding</title>
-    <meta name="description" content="Le site de planification de mariage">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>MyWedding</title>
+		<meta name="description" content="Le site de planification de mariage">
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../view/css/inscription.css" type="text/css" />
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="../view/css/inscription.css" type="text/css" />
 
-  </head>
+	</head>
 
-  <body>
-    <header>
-      <div class="form-signin-heading col-xs-12">
-        <center><h1>Bienvenue sur MyWedding</h1><center/>
-      </div>
+	<body>
+		<header>
+			<h1>Bienvenue sur MyWedding</h1>
+		</header>
 
-    </header>
-    <div class="container col-xs-12 col-sm-6" >
-      <form class="form-signin" action="connecter.ctrl.php" method="get">
-        <h2 class="form-signin-heading">Connectez-vous</h2>
+		<main>
+			<div id="container">
+				<form class="form-signin" action="connecter.ctrl.php" method="get">
+					<h2 class="form-signin-heading">Connectez-vous</h2>
 
-        <label for="inputEmail" class="sr-only">Adresse mail</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required autofocus>
+					<label for="inputEmail" class="sr-only">Adresse mail</label>
+					<input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required autofocus>
 
-        <label for="inputPassword" class="sr-only">Mot de passe</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+					<label for="inputPassword" class="sr-only">Mot de passe</label>
+					<input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
 
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Se souvenir de moi
-          </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
-      </form>
-    </div> <!-- /container -->
+					<div class="checkbox">
+						<label>
+							<input type="checkbox" value="remember-me"> Se souvenir de moi
+						</label>
+					</div>
+					<button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+				</form>
 
 
-    <div class="container col-xs-12 col-sm-6">
-      <form class="form-signin" action="inscrire.ctrl.php" method="get">
-        <h2 class="form-signin-heading">Inscrivez-vous</h2>
+				<form class="form-signin" action="inscrire.ctrl.php" method="get">
+					<h2 class="form-signin-heading">Inscrivez-vous</h2>
 
-        <label for="inputNom" class="sr-only">Nom</label>
-        <input type="text" id="inputNom" class="form-control" placeholder="Nom" required autofocus>
+					<label for="inputNom" class="sr-only">Nom</label>
+					<input type="text" id="inputNom" class="form-control" placeholder="Nom" required autofocus>
 
-        <label for="inputPrenom" class="sr-only">Prenom</label>
-        <input type="text" id="inputPrenom" class="form-control" placeholder="Prenom" required>
+					<label for="inputPrenom" class="sr-only">Prenom</label>
+					<input type="text" id="inputPrenom" class="form-control" placeholder="Prenom" required>
 
-        <label for="inputEmail" class="sr-only">Adresse mail</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required>
+					<label for="inputEmail" class="sr-only">Adresse mail</label>
+					<input type="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required>
 
-        <label for="inputPassword" class="sr-only">Mot de passe</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+					<label for="inputPassword" class="sr-only">Mot de passe</label>
+					<input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Inscription</button>
-      </form>
-    </div> <!-- /container -->
+					<button class="btn btn-lg btn-primary btn-block" type="submit">Inscription</button>
+				</form>
+			</div>
+		</main>
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+	</body>
 </html>
