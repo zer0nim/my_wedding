8ccaf673db9b72cc31990d100c5f2159414f27bc
dms97 <damiendecrossey@hotmail.fr>
2016-11-21 22:45:48 +0100

    fdp de git

--- view/invitations.view.php
+++ view/invitations.view.php
@@ -5,11 +5,7 @@
 
 <div class="col-sm-12">
   <form class="" action="" method="post">
-<<<<<<< HEAD
     <textarea name="texteInvit" wrap="soft" ><?=$texte?></textarea>
-=======
-    <textarea name="texteInvit" wrap="soft" ></textarea>
->>>>>>> origin/master
     <div class="row">
         <button class="col-sm-2 col-sm-offset-2 btn btn-primary" >Enregistrer</button>
         <button class="col-sm-2 col-sm-offset-1 btn btn-primary" >Envoyer par mail</button>
