e47af1cd851f2df9cf8e27c53ad3f2c4d0ebaa8b
zer0nim <marinernes@hotmail.fr>
2016-11-23 14:39:53 +0100

    work on contact

--- view/contacts.view.php
+++ view/contacts.view.php
@@ -22,11 +22,51 @@ require_once '../view/baseMenuFnct.php';
 				<?php printAllContacts($allContacts); ?>
 			</select>
 			<!-- -^- Liste contacts-^- -->
-			<script>
+			<script>/*
+			function loadName(id) {
+			    var xhr = new XMLHttpRequest();
+
+			    // On souhaite juste récupérer le contenu du fichier, la méthode GET suffit amplement :
+			    xhr.open('GET', file);
+
+			    xhr.addEventListener('readystatechange', function() { // On gère ici une requête asynchrone
+
+			        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) { // Si le fichier est chargé sans erreur
+
+			            document.getElementById('fileContent').innerHTML = '<span>' + xhr.responseText + '</span>'; // Et on affiche !
+
+			        }
+
+			    });
+
+			    xhr.send(null); // La requête est prête, on envoie tout !
+
+			}
+			(function() { // Comme d'habitude, une IIFE pour éviter les variables globales
+
+			    var inputs = document.getElementsByTagName('input'),
+			        inputsLen = inputs.length;
+
+			    for (var i = 0; i < inputsLen; i++) {
+
+			        inputs[i].addEventListener('click', function() {
+			            loadFile(this.value); // À chaque clique, un fichier sera chargé dans la page
+			        });
+
+			    }
+
+			})();
+			//controller\ajax\contact.ajax.php
 			$('#select-cnt').bind('input', function() {
+				var idcont = $(this).val();
+				if (idcont.length == 1) {
+					document.getElementById("NomLink").value = " <?php echo $allContacts[1]->getCont_nom(); ?>"; // get the current value of the input field.
+				}
+				else { //a faire: griser la partie droite de lecran
 					document.getElementById("NomLink").value = $(this).val(); // get the current value of the input field.
+				}
 			});
-			</script>
+			*/</script>
 
 		</div>
 		<!--
