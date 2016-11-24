9cd7d233933bebaa3da2548ba072508d402ad879
DragonSkyMine <dragonskymine@gmail.com>
2016-11-24 21:11:10 +0100

    visuel ListeSouhait

--- view/liste.view.php
+++ view/liste.view.php
@@ -4,14 +4,15 @@
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <link rel="stylesheet" href="../view/css/liste.css" type="text/css" />
 
-	<div class="box col-sm-6 col-md-4">
-		<div class="demo">
+	<div class="box col-sm-6 col-md-4 col-sm-offset-3 col-md-offset-4 " style="background-color:grey">
+		<div class="liste">
 			<ul id="sortable">
 				<?php // Affichage de la liste
 					foreach ($data as $key => $value) { echo '<li id="list_' . $value['nom'] . '" class="list-group-item ui-state-default" type="button">' . $value['nom'] . '</li>'
 						;	}
 				?>
 			</ul>
+			<input type="text" placeholder="nouveau souhait ?" width="">
 		</div>
 	<div class="box col-sm-6 col-md-4">
 
