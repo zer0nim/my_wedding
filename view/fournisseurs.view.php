509075bd89f589c83ae6c3e856f59ca94e22cddd
DragonSkyMine <dragonskymine@gmail.com>
2016-11-24 21:45:27 +0100

    Remove : Commentaire de la page non fonctionnel pour gain de place

--- view/fournisseurs.view.php
+++ view/fournisseurs.view.php
@@ -5,60 +5,6 @@
 
 
 <div class="row">
-
-	<!--<div class="box col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<div class="caption">
-				<address>
-					<strong>Sono et Eclairage</strong><br>
-					Notre Dame, Paris, France<br>
-					Téléphone : 0653783195<br>
-					Adresse Mail : contact@sono3000.net<br>
-					Site Internet : www.sono3000.net
-				</address>
-				<blockquote>
-					<p>Livraison et installation de la sono et des éclairages mercredi 10 heure</p>
-				</blockquote>
-				<p><a href="#" class="btn btn-danger" role="button">Supprimer</a></p>
-			</div>
-		</div>
-	</div>
-
-	<div class="box col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<div class="caption">
-				<address>
-					<strong>Clé de la salle</strong><br>
-					Avenue du Stade de France, Saint-Denis, France<br>
-					Téléphone : 0934163575<br>
-					Adresse Mail : reservation@salledespectacles.com<br>
-					Site Internet : www.salledespectacles.com
-				</address>
-				<blockquote>
-					<p>Aller chercher les clés de la salle lundi dans la matinée </p>
-				</blockquote>
-				<p><a href="#" class="btn btn-danger" role="button">Supprimer</a></p>
-			</div>
-		</div>
-	</div>
-
-	<div class="box col-sm-6 col-md-4">
-		<div class="thumbnail">
-			<div class="caption">
-				<address>
-					<strong>Traiteur</strong><br>
-					Paris Gare de Lyon, Place Louis Armand, Paris, France<br>
-					Téléphone : 0657563135<br>
-					Adresse Mail : contact@traiteur2000.fr<br>
-					Site Internet : www.traiteur2000.fr
-				</address>
-				<blockquote>
-					<p>Arrivée traiteur jeudi vers 20 heure 30</p>
-				</blockquote>
-				<p><a href="#" class="btn btn-danger" role="button">Supprimer</a></p>
-			</div>
-		</div>
-	</div>-->
 	<?php
 		foreach ($data['fournisseurs'] as $key => $value) {
 			$value->afficherFournisseur();
