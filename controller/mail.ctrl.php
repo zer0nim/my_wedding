088befae08366dfabc783f59a1c80e3c9bd90cf0
dms97 <mariotto.damien@gmail.com>
2016-12-03 22:26:22 +0100

    test mail

--- controller/mail.ctrl.php
+++ controller/mail.ctrl.php
@@ -2,26 +2,56 @@
   require_once('../model/DAO.class.php');
   //////////VERSION SANS SESSION//////////
   $idM=1;
+  $nom='un inconnu qui test';
   ////////////////////////////////////////
   if (isset($_POST['actSend'])) {
-    if(!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)){
-      $passage_ligne = "\r\n";
+    $mail = $_POST['for']; // Déclaration de l'adresse de destination.
+    if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)){ // On filtre les serveurs qui présentent des bogues.
+    	$passage_ligne = "\r\n";
     }else{
-      $passage_ligne = "\n";
+    	$passage_ligne = "\n";
     }
+    //=====Déclaration des messages au format HTML.
+    $message_html = $_POST['message'];
+    //==========
 
-    $header = "From: \"$nom\"<$email>".$passage_ligne; //création du header du mail
-    $header.= "Reply-to: \"$nom\" <$email>".$passage_ligne;
-    $header.= "MIME-Version: 1.0".$passage_ligne;
-    $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
-
-    $message = "...";
-    $message .= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
-    $message .= "Content-Transfer-Encoding: 8bit".$passage_ligne;
-    $message .= "...";
-
+    //=====Création de la boundary.
     $boundary = "-----=".md5(rand());
-  }else {
-    include("../view/mail.view.php");
+    $boundary_alt = "-----=".md5(rand());
+    //==========
+
+    //=====Définition du sujet.
+    $sujet = $_POST['obj'];
+    //=========
+
+    //=====Création du header de l'e-mail.
+    $header = "From: \"$nom\"<$mail>".$passage_ligne;
+    $header.= "Reply-to: \"$nom\" <$mail>".$passage_ligne;
+    $header.= "MIME-Version: 1.0".$passage_ligne;
+    $header.= "Content-Type: multipart/mixed;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+    //==========
+
+    //=====Création du message.
+    $message = $passage_ligne."--".$boundary.$passage_ligne;
+    $message.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary_alt\"".$passage_ligne;
+    $message.= $passage_ligne."--".$boundary_alt.$passage_ligne;
+
+    //=====Ajout du message au format HTML.
+    $message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+    $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+    $message.= $passage_ligne.$message_html.$passage_ligne;
+    //==========
+
+    //=====On ferme la boundary alternative.
+    $message.= $passage_ligne."--".$boundary_alt."--".$passage_ligne;
+    //==========
+
+    $message.= $passage_ligne."--".$boundary.$passage_ligne;
+
+    //=====Envoi de l'e-mail.
+    mail($mail,$sujet,$message,$header);
+    //==========
   }
+
+  include("../view/mail.view.php");
  ?>
--- view/mail.view.php
+++ view/mail.view.php
@@ -4,6 +4,13 @@
 <?php
 	require_once '../view/baseMenuFnct.php';
 	$texte=$dao->getInvitation($idM);
+	if (isset($_POST['actSend'])) {
+?>
+		<div class="alert alert-success">
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+			<strong>Mail envoyé !</strong>
+		</div>
+<?php }
 ?>
 
   <form class="" action="mail.ctrl.php" method="post">
@@ -12,19 +19,26 @@
       <div class="form-group">
         <label for="from" class="col-sm-1">From : </label>
         <div class="col-sm-6">
-          <input type="email" id="from" class="form-control" placeholder="example@mail.com" required>
+          <input type="email" name="from" id="from" class="form-control" placeholder="example@mail.com" required autofocus>
         </div>
       </div>
 
       <div class="form-group">
         <label for="for" class="col-sm-1">For : </label>
         <div class="col-sm-6">
-          <input type="email" id="for" class="form-control" placeholder="example@mail.com" required>
+          <input type="email" id="for" name="for" class="form-control" placeholder="example@mail.com" required>
+        </div>
+      </div>
+
+			<div class="form-group">
+        <label for="obj" class="col-sm-1">Object : </label>
+        <div class="col-sm-6">
+          <input type="text" id="obj" name="obj" class="form-control" value="sans objet" required>
         </div>
       </div>
 
       <div class="form-group">
-        <label for="message" class="col-sm-12">Message : </label>
+        <label for="message" class="col-sm-12">Message (with HTML beacon) : </label>
         <div class="col-sm-7">
           <textarea name="message" id="message" class="form-control" rows="15" cols="200"><?=$texte?></textarea>
         </div>
