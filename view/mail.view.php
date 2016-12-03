779d946fb456d294d2f04c6a023e266efd5fe61e
dms97 <mariotto.damien@gmail.com>
2016-12-03 19:49:15 +0100

    encore un

--- view/mail.view.php
+++ view/mail.view.php
@@ -8,30 +8,30 @@
 
   <form class="" action="mail.ctrl.php" method="post">
 
-    <div class="form-horizontal col-sm-offset-4">
+    <div class="form-horizontal col-sm-offset-3">
       <div class="form-group">
         <label for="from" class="col-sm-1">From : </label>
-        <div class="col-sm-5">
+        <div class="col-sm-6">
           <input type="email" id="from" class="form-control" placeholder="example@mail.com" required>
         </div>
       </div>
 
       <div class="form-group">
         <label for="for" class="col-sm-1">For : </label>
-        <div class="col-sm-5">
+        <div class="col-sm-6">
           <input type="email" id="for" class="form-control" placeholder="example@mail.com" required>
         </div>
       </div>
 
       <div class="form-group">
         <label for="message" class="col-sm-12">Message : </label>
-        <div class="col-sm-6">
+        <div class="col-sm-7">
           <textarea name="message" id="message" class="form-control" rows="15" cols="200"><?=$texte?></textarea>
         </div>
       </div>
 
       <div class="row">
-        <button class="col-sm-6 btn btn-primary" name="actSend">Envoyer</button>
+        <button class="col-sm-7 btn btn-primary" name="actSend">Envoyer</button>
       </div>
     </div>
 
