22c73442d1d7915c8c071a6459ea6606c09fe556
DragonSkyMine <dragonskymine@gmail.com>
2016-12-09 15:23:56 +0100

    intégration session (suite)

--- controller/fournisseurs.ctrl.php
+++ controller/fournisseurs.ctrl.php
@@ -1,6 +1,7 @@
 <?php
+  include_once('session_gestion.ctrl.php');
   require_once('../model/DAO.class.php');
-  $idM = 1;
+  $idM = $_SESSION['idM'];
 
   // Pour suppression d'un fournisseur
   if (isset($_GET['idS']) && (isset($_GET['idM']))) {
--- controller/session_gestion.ctrl.php
+++ controller/session_gestion.ctrl.php
@@ -1,3 +1,4 @@
 <?php
   session_start();
+  $_SESSION['idM'] = 1;
 ?>
