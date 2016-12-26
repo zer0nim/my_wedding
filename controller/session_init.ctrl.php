e1cc7f82815c4662034318881eb5772f197493a3
DragonSkyMine <dragonskymine@gmail.com>
2016-12-26 16:12:58 +0100

    correction problème session

--- controller/session_init.ctrl.php
+++ controller/session_init.ctrl.php
@@ -1,6 +1,9 @@
 <?php
   session_start();
+  require_once('../model/DAO.class.php');
   //remplire les valeur avec celle de la bdd
   $_SESSION['account'] = $info[0];
   $_SESSION['idM'] = $info[1];
+  $req = $dao->getMariage($_SESSION['idM']);
+  $_SESSION['date'] = req[0];
 ?>
