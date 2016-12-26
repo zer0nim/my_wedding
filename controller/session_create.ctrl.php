c7535fed26b9569fe1f4429976ea6d6ff11ac2fc
DragonSkyMine <dragonskymine@gmail.com>
2016-12-26 16:10:54 +0100

    ajout session date

--- controller/session_create.ctrl.php
+++ controller/session_create.ctrl.php
@@ -1,6 +1,9 @@
 <?php
   session_start();
+  require_once('../model/DAO.class.php');
   $_SESSION['idM'] = 1;
   $_SESSION['account'] = 1;
+  $req = $dao->getMariage($_SESSION['idM']);
+  $_SESSION['date'] = req[0];
   require_once('session_gestion.ctrl.php');
 ?>
