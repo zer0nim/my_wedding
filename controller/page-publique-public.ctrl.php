cf789109305ee3b2ea3012457f11dcabb3b2c883
dms97 <mariotto.damien@gmail.com>
2016-12-30 15:05:33 +0100

    modif

--- controller/page-publique-public.ctrl.php
+++ controller/page-publique-public.ctrl.php
@@ -1,3 +1,19 @@
 <?php
-include_once('../view/page-publique-public.view.php');
- ?>
+  include_once('../model/DAO.class.php');
+  include_once('session_create.ctrl.php');
+  
+  $idM=$_SESSION['idM'];
+
+
+  if (isset($_POST['envoiQuestion'])) {
+    $nom=$_POST['nom'];
+    $question=$_POST['question'];
+    $date = date("Y-m-d H:i:s");
+    $dao->envoiquestion($idM,$nom,$question,$date);
+  }
+  $questions=$dao->getquestions($idM);
+  if ($questions==NULL) {
+    $questions=0;
+  }
+  include_once('../view/page-publique-public.view.php');
+?>
