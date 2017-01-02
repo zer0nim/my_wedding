3bc91144fdec375beeb3600ed0ed0b57eafba253
zer0nim <marinernes@hotmail.fr>
2017-01-02 10:12:11 +0100

    Compteur fonctionne par rapport a la date du mariage

--- view/js/accueil.js
+++ view/js/accueil.js
@@ -1,14 +1,33 @@
-$('#wedding').countdown({
-	date: '02/27/2017 14:00:00',
-	offset: +2,
-	day: 'Jour',
-	days: 'Jours',
-	hour: 'Heure',
-	hours: 'Heures',
-	minute: 'Minute',
-	minutes: 'Minutes',
-	second: 'Seconde',
-	seconds: 'Secondes'
-}, function () {
-	alert('Que la cérémonie commence !');
+$(document).ready(function(){
+	$.post(
+			'../controller/ajax_get_wed_hour.php', // Le fichier cible côté serveur.
+			{
+					idcont : $('#select-cnt').val()
+			},
+
+			function(data){
+				startCountdown(data);
+			},
+
+			'text' // Format des données reçues.
+	);
 });
+
+function startCountdown(dateM) {
+	$.getScript("../model/cntDown/jquery.countdown.min.js?v=1.0.0.0", function(){
+		$('#wedding').countdown({
+			date: dateM,
+			offset: +2,
+			day: 'Jour',
+			days: 'Jours',
+			hour: 'Heure',
+			hours: 'Heures',
+			minute: 'Minute',
+			minutes: 'Minutes',
+			second: 'Seconde',
+			seconds: 'Secondes'
+		}, function () {
+			alert('Que la cérémonie commence !');
+		});
+	});
+}
--- view/scripts.php
+++ view/scripts.php
@@ -2,7 +2,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script src="https://use.fontawesome.com/f332948f4d.js"></script>
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script type="text/javascript" src="../model/cntDown/jquery.countdown.min.js?v=1.0.0.0"></script>
 <!-- Bootstrap Date-Picker Plugin -->
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/locales/bootstrap-datepicker.fr.min.js"></script>
