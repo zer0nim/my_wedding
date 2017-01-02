faeb8c9cefae81cfbd27d38531ca8ed4a93d6b20
GE3MS <guige3m.s@gmail.com>
2017-01-02 19:57:24 +0100

    sdml

--- view/inscription.view.php
+++ view/inscription.view.php
@@ -14,48 +14,68 @@
 
 	<body>
 		<header>
-			<a href="inscription.ctrl.php">
+			<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+							<img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image">
+						<a class="navbar-brand" href="inscription.ctrl.php">MyWedding</a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#sign" class="btn btn-primary navbar-btn" role="button">S'inscrire</a></li>
+							<li><a href="#sign" class="btn btn-default navbar-btn" role="button">Se connecter</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
 				<div>
 					<div class="col-sm-6">
 						<h1>MyWedding:</h1>
 						<h2>Le meilleur moyen d'organiser votre mariage</h2>
 					</div>
 				</div>
-			</a>
-
-			<?php if (isset($messErr)) {?>
-					<div class="alert alert-danger">
-						<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-						<strong>Erreur : </strong><?=$messErr?>
-					</div>
-			<?php } ?>
-			<div id="sign">
-				<form class="form-signin" action="inscription.ctrl.php" method="post">
-					<h2 class="form-signin-heading">Connexion</h2>
-
-					<label for="inputEmail" class="sr-only">Adresse mail</label>
-					<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required>
-
-					<label for="inputPassword" class="sr-only">Mot de passe</label>
-					<input type="password" name="motdepasse" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-
-					<button class="btn btn-lg btn-primary btn-block" name="connexion" type="submit">Se connecter</button>
-				</form>
-
-
-				<form class="form-signin" action="inscription.ctrl.php" method="post">
-					<h2 class="form-signin-heading">Inscription</h2>
-
-					<label for="inputEmail" class="sr-only">Adresse mail</label>
-					<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required>
-
-					<label for="inputPassword" class="sr-only">Mot de passe</label>
-					<input type="password" name="motdepasse" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
-
-					<button class="btn btn-lg btn-primary btn-block" name="inscription" type="submit">S'inscrire</button>
-				</form>
-			</div>
 		</header>
+		<?php if (isset($messErr)) {?>
+				<div class="alert alert-danger">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+					<strong>Erreur : </strong><?=$messErr?>
+				</div>
+		<?php } ?>
+		<div id="sign">
+			<form class="form-signin" action="inscription.ctrl.php" method="post">
+				<h2 class="form-signin-heading">Connexion</h2>
+
+				<label for="inputEmail" class="sr-only">Adresse mail</label>
+				<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required>
+
+				<label for="inputPassword" class="sr-only">Mot de passe</label>
+				<input type="password" name="motdepasse" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+
+				<button class="btn btn-lg btn-primary btn-block" name="connexion" type="submit">Se connecter</button>
+			</form>
+
+
+			<form class="form-signin" action="inscription.ctrl.php" method="post">
+				<h2 class="form-signin-heading">Inscription</h2>
+
+				<label for="inputEmail" class="sr-only">Adresse mail</label>
+				<input type="email" name="email" id="inputEmail" class="form-control" placeholder="Adresse mail" required>
+
+				<label for="inputPassword" class="sr-only">Mot de passe</label>
+				<input type="password" name="motdepasse" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+
+				<button class="btn btn-lg btn-primary btn-block" name="inscription" type="submit">S'inscrire</button>
+			</form>
+		</div>
 
 			<div id="features" class="container">
 				<div class="col-sm-4">
