2485316e2cdcd146eea3e3d97c8b422110b8380c
zer0nim <marinernes@hotmail.fr>
2017-01-03 19:37:22 +0100

    Corection bug affichage bouton plan de table

--- view/plandetable.view.php
+++ view/plandetable.view.php
@@ -12,7 +12,10 @@
 		<div class="nopadding form-group col-xs-12 col-lg-6">
 			<button name="singlebutton" class="btn btn-primary btn-block">Placement automatique</button>
 		</div>
-		<a href="pdf_plandetable.ctrl.php" class="btn btn-primary btn-block">Telecharger</a>
+		<div class="nopadding form-group col-xs-12 col-lg-6">
+			<a href="pdf_plandetable.ctrl.php" class="btn btn-primary btn-block">Telecharger</a>
+		</div>
+
 	</div>
 	<!-- -^- Génerer -^- -->
 
