9999cb0422f2dc8ecd54546c14f8b42ac028c7ce
zer0nim <marinernes@hotmail.fr>
2017-01-05 16:46:12 +0100

    Modif visuel tchat

--- view/public-question.view.php
+++ view/public-question.view.php
@@ -1,5 +1,6 @@
 <?php include('../view/header.php') ?>
 <link rel="stylesheet" href="../view/css/public-question.css" type="text/css" />
+
 </head>
 <body class="container-fluid">
 
@@ -34,54 +35,56 @@
 				<strong>Message non envoyé : </strong><?=$erreur?>
 			</div>
 	<?php } ?>
-	<div class="questions col-xs-12">
-		<legend>Questions pour les organisateurs du mariage</legend>
-		<div id="scrollable" class="col-xs-12">
-			<?php
-			if ($questions==0) {
-				echo "<p>Pas de questions pour l'instant</p>\n";
-			}else{
-				$nb=count($questions);
-				for ($i=0; $i < $nb; $i++) {
-					if ($questions[$i]['quest_nom']=='Organisateur') {
-						echo "<strong class=\"admin\">{$questions[$i]['quest_nom']}: </strong><p>{$questions[$i]['quest_question']}</p> <p class='date'>{$questions[$i]['quest_date']}</p>\n";
-					}else {
-						echo "<strong>{$questions[$i]['quest_nom']}: </strong><p>{$questions[$i]['quest_question']}</p> <p class='date'>{$questions[$i]['quest_date']}</p>\n";
-					}
-					if($i<$nb-1){
-						echo "<hr>\n";
+	<div class="col-lg-offset-2 col-lg-8">
+		<div class="questions col-xs-12">
+			<legend>Questions pour les organisateurs du mariage</legend>
+			<div id="scrollable" class="col-xs-12">
+				<?php
+				if ($questions==0) {
+					echo "<p>Pas de questions pour l'instant</p>\n";
+				}else{
+					$nb=count($questions);
+					for ($i=0; $i < $nb; $i++) {
+						if ($questions[$i]['quest_nom']=='Organisateur') {
+							echo "<strong class=\"admin\">{$questions[$i]['quest_nom']}: </strong><p>{$questions[$i]['quest_question']}</p> <p class='date'>{$questions[$i]['quest_date']}</p>\n";
+						}else {
+							echo "<strong>{$questions[$i]['quest_nom']}: </strong><p>{$questions[$i]['quest_question']}</p> <p class='date'>{$questions[$i]['quest_date']}</p>\n";
+						}
+						if($i<$nb-1){
+							echo "<hr>\n";
+						}
 					}
 				}
-			}
-			?>
-			<script>
-				element = document.getElementById('scrollable');
-				element.scrollTop = element.scrollHeight;
-			</script>
+				?>
+				<script>
+					element = document.getElementById('scrollable');
+					element.scrollTop = element.scrollHeight;
+				</script>
+			</div>
+
+			<form class="form-horizontal" action="public-question.ctrl.php?idm=<?=$idM?>" method="post">
+				<div class="form-group">
+					<label for="nom" class="control-label col-xs-12">Nom/Prenom :</label>
+					<div class="col-xs-12">
+						<input id="nom" type="text" name="nom" class="form-control" required>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="question" class="col-xs-12">Question :</label>
+					<div class="col-xs-12">
+						<input id="question" type="text" name="question" class="form-control" required>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="col-xs-12">
+						<button class="btn btn-secondary" name="envoiQuestion">Envoyer</button>
+					</div>
+				</div>
+
+			</form>
 		</div>
-
-		<form class="form-horizontal" action="public-question.ctrl.php?idm=<?=$idM?>" method="post">
-			<div class="form-group">
-				<label for="nom" class="control-label col-sm-2 col-lg-1">Nom/Prenom :</label>
-				<div class="col-sm-10 col-lg-11">
-					<input id="nom" type="text" name="nom" class="form-control" required>
-				</div>
-			</div>
-
-			<div class="form-group">
-				<label for="question" class="control-label col-sm-1">Question :</label>
-				<div class="col-sm-11">
-					<input id="question" type="text" name="question" class="form-control" required>
-				</div>
-			</div>
-
-			<div class="form-group">
-				<div class="col-xs-12">
-					<button class="btn btn-secondary" name="envoiQuestion">Envoyer</button>
-				</div>
-			</div>
-
-		</form>
 	</div>
 
 <?php
