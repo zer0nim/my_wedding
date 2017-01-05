4cbca02e71cb1f0a7a8c0810e2fd2285662f70a0
DragonSkyMine <dragonskymine@gmail.com>
2017-01-05 23:55:16 +0100

    finition liste

--- controller/liste.ctrl.php
+++ controller/liste.ctrl.php
@@ -8,15 +8,11 @@
 
   function print_list($list) {
     if (isset($list)) {
-      if (count($list) == 0) {
-        echo "Vous n'avez pas encore fait de voeux";
-      } else {
-        foreach ($list as $key => $value) {
-          echo '<li id="list_' . $value['nom'] . '" class="list-group-item ui-state-default">' . $value['nom'] . '	<a class="supr-souh btn btn-danger btn-xs" role="button"><i class="fa fa-times" aria-hidden="true"></i></a></li>';
-        }
+      foreach ($list as $key => $value) {
+        echo '<li id="list_' . $value['nom'] . '" class="list-group-item ui-state-default">' . $value['nom'] . '	<a class="supr-souh btn btn-danger btn-xs" role="button"><i class="fa fa-times" aria-hidden="true"></i></a></li>';
       }
-    } else {
+    } /*else {
       echo "Vous n'avez pas encore fait de voeux";
-    }
+    }*/
   }
 ?>
